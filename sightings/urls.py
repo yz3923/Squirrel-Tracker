89d71b9b1d728fc39471feff0fdd65210f1c3e28
Julia Xia <yx2648@columbia.edu>
2021-04-13 15:46:46 +0000

    added path to views/functions

---
+++ sightings/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.all, name="index"),
+    path('add/', views.add, name="add"),
+    path('stats/', views.stats, name="stats"),
+    path('<unique_squirrel_id>/', views.edit, name="edit"),
+]
