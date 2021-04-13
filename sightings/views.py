43cdc1cebd49fa262ffe0a9185c13540b3755269
Yangyi Zheng <yz3923@columbia.edu>
2021-04-13 13:58:14 +0000

    update made

--- sightings/views.py
+++ sightings/views.py
@@ -1,22 +1,29 @@
 from django.http import HttpResponse
 
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 
 from django.core.paginator import Paginator
 
-from .models import Squirrels
+from map.models import Squirrel
+
+from .forms import form_sightings
 
 def show_all(request):
     squirrels = Squirrel.objects.all()
     context = {'squirrels' : squirrels}
     return render (request, 'sightings/show_all.html', context)
 
-def create(request):
+def add(request):
     if request.method == 'POST':
         form = form_sightings(request.POST)
         if form.is_valid():
             form.save()
-            return render('/sightings')
+            return redirect('/sightings/')
+        else:
+            form = form_sightings()
+        return render(request, 'sightings/add.html', {'form':form})
+
+
 
 
 
