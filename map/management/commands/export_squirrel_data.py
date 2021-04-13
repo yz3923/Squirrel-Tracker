f6f2a044ec1e34e22d8a03463ee40af79809210b
Yangyi Zheng <yz3923@columbia.edu>
2021-04-13 11:51:22 +0000

    modified

--- map/management/commands/export_squirrel_data.py
+++ map/management/commands/export_squirrel_data.py
@@ -5,12 +5,10 @@ from map.models import Squirrel
 import sys
 
 import csv
+import control
 
 class Command(BaseCommand):
     help = 'export squirrel data'
-
-    def add_arguments(self, parser):
-        parser.add_argument('path', type=str)
 
     def handle(self, path, **kwargs):
         with open(path, 'w', newline='') as f:
@@ -20,3 +18,5 @@ class Command(BaseCommand):
             for i in Squirrel.objects.all():
                 writer.writerow([getattr(i, j) for j in field])
 
+    def add_arguments(self, parser):
+        parser.add_argument('path', type=str)
