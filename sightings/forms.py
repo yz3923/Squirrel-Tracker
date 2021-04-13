93ce36518b496ac19c5d31591a907a69bb28a199
Julia Xia <yx2648@columbia.edu>
2021-04-13 15:56:30 +0000

    add forms

---
+++ sightings/forms.py
@@ -0,0 +1,11 @@
+from django.forms import ModelForm
+
+from map.models import Squirrel
+
+
+class SquirrelForm(ModelForm):
+    class Meta:
+        model = Squirrel
+        fields = '__all__'
+        labels = {
+                "unique_squirrel_id": "unique_squirrel_id(cannot be blank)"}              
