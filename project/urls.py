1a689f5204fee8d40d08eaca8823c44e9f3eeee8
Julia Xia <yx2648@columbia.edu>
2021-04-14 01:11:51 +0000

    added sightings and home to urls

--- project/urls.py
+++ project/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('',views.home),
     path('admin/', admin.site.urls),
-    path('map/', include('map.urls'))
+    path('map/', include('map.urls')),
+    path('sightings/', include('sightings.urls')),
 ]
